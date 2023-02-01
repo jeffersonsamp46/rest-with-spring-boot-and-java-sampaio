5deb22b7951b34161ea6309140808ce45fdc94fa
jeffersonsamp46 <46848132+jeffersonsamp46@users.noreply.github.com>
2023-01-31 21:27:06 -0300

    primeiro commit

---
+++ JavaSpringBoot/rest-with-spring-boot-and-java-sampaio/src/test/java/br/com/sampaio/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.sampaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
